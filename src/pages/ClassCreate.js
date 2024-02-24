07c5c13444998798fa17ae2bb7eb5a613c137d44
Ana Testemitanu <annatestemitanu@gmail.com>
2024-02-24 15:48:58 +0000

    Level of the class amended

--- src/pages/ClassCreate.js
+++ src/pages/ClassCreate.js
@@ -4,7 +4,7 @@ import "../styles/dashboard.css"
 import "../styles/content.css"
 import "../styles/ClassCreate.css"
 import Sidebar from '../components/Sidebar';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const ClassCreate = () => {
   const [classData, setClassData] = useState({
@@ -20,7 +20,7 @@ const ClassCreate = () => {
     analogy: false 
   })
 
-  let history = useHistory();
+  let navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setClassData({ ...classData, [e.target.name]: e.target.value })
@@ -38,14 +38,14 @@ const ClassCreate = () => {
     e.preventDefault()
     // Perform operations with classData here 
     // (send it to the server or whatever needs to be done)
-    history.push('/previousclasses');
+    navigate('/previousclasses');
   }
 
   return (
     <div className="form-container">
     <Sidebar />
         <form className="form" onSubmit={handleSubmit}>
-        <label className="form-input-label"> 
+        <label className="form-input-label">
             Name of class:
             <input className="form-input-text" type="text" name="name" onChange={handleInputChange} />
         </label>
@@ -59,40 +59,43 @@ const ClassCreate = () => {
         </label>
         <label className="form-input-label">
             Level of the class:
-            <input className="form-input-text" type="text" name="level" onChange={handleInputChange} />
+            <select name="level" onChange={handleInputChange}>
+                <option value="">Choose a level...</option>
+                <option value="1">Lower Ability</option>
+                <option value="2">Medium Ability</option>
+                <option value="3">High Ability</option>
+            </select>
         </label>
         <label className="form-input-label">
             Age of class:
             <input className="form-input-text" type="text" name="age" onChange={handleInputChange} />
         </label>
         <div style={{textAlign: 'left'}}>
-        <label className="form-input-label">
-            Tons
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}}>
-                <div style={{textAlign: 'center'}}>
-                    <label className="form-input-label">Humour</label>
-                    <input type="checkbox" name="humour" onChange={handleCheckboxChange} style={{display: 'block', margin: '0 auto', transform: 'scale(1.5)'}} />
+            <label className="form-input-label">
+                Tons
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                    <div style={{display: 'flex', justifyContent: 'space-between', width: '200px'}}>
+                        <div style={{textAlign: 'center'}}>
+                            <label className="form-input-label">Humour</label>
+                            <input type="checkbox" name="humour" onChange={handleCheckboxChange} style={{display: 'block', margin: '0 auto', transform: 'scale(1.5)'}} />
+                        </div>
+                        <div style={{textAlign: 'center'}}>
+                            <label className="form-input-label">Analogy</label>
+                            <input type="checkbox" name="analogy" onChange={handleCheckboxChange} style={{display: 'block', margin: '0 auto', transform: 'scale(1.5)'}} />
+                        </div>
+                    </div>
                 </div>
-
-                <div style={{textAlign: 'center'}}>
-                    <label className="form-input-label">Analogy</label>
-                    <input type="checkbox" name="analogy" onChange={handleCheckboxChange} style={{display: 'block', margin: '0 auto', transform: 'scale(1.5)'}} />
-                </div>
-
-                </div>
-            </div>
-        </label>
+            </label>
         </div>
         <label className="form-input-label">
             Student group name:
             <input className="form-input-text" type="text" name="studentGroupName" onChange={handleInputChange} />
         </label>
         <div style={{textAlign: 'center'}}>
-        <label className="form-input-label">
-            Mark as "To share"
-            <input type="checkbox" name="toShare" onChange={handleCheckboxChange} style={{display: 'block', margin: '0 auto', transform: 'scale(1.5)'}} />
-        </label>
+            <label className="form-input-label">
+                Mark as "To share"
+                <input type="checkbox" name="toShare" onChange={handleCheckboxChange} style={{display: 'block', margin: '0 auto', transform: 'scale(1.5)'}} />
+            </label>
         </div>
         <button type="submit">Submit</button>
         </form>
